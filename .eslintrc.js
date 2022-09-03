ddc068e4e61e57c801f42b0703d6b22bddaa57d8
Liang Yong <LiangDongXing.cn@outlook.com>
2022-09-03 19:00:31 +0800

    加入前端代码格式化 ESLint：语法检测工具 Prettier：代码风格控制工具

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-strongly-recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {
+    "prettier/prettier": ["error"],
+
+    /**
+     "off" 或 0 - 关闭规则
+     "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+     "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+     */
+  },
+};
