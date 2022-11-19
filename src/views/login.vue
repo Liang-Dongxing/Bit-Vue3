156503e6290e49b039ced61f2278ef4c2512a336
Liang Yong <LiangDongXing.cn@outlook.com>
2022-11-19 11:46:04 +0800

    登录页和注册页面本地化翻译完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -5,21 +5,21 @@
         <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
           <h3 class="title">{{ appTitle }}</h3>
           <el-form-item prop="username">
-            <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
+            <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" :placeholder="$t('om.login.username')">
               <template #prefix>
                 <svg-icon icon-class="user" class="el-input__icon input-icon" />
               </template>
             </el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" placeholder="密码" @keyup.enter="handleLogin">
+            <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" :placeholder="$t('om.login.password1')" @keyup.enter="handleLogin">
               <template #prefix>
                 <svg-icon icon-class="password" class="el-input__icon input-icon" />
               </template>
             </el-input>
           </el-form-item>
           <el-form-item prop="code" v-if="captchaEnabled">
-            <el-input v-model="loginForm.code" size="large" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter="handleLogin">
+            <el-input v-model="loginForm.code" size="large" auto-complete="off" :placeholder="$t('om.login.code')" style="width: 63%" @keyup.enter="handleLogin">
               <template #prefix>
                 <svg-icon icon-class="validCode" class="el-input__icon input-icon" />
               </template>
@@ -28,14 +28,14 @@
               <img :src="codeUrl" @click="getCode" class="login-code-img" />
             </div>
           </el-form-item>
-          <el-checkbox v-model="loginForm.rememberMe" style="margin: 0px 0px 25px 0px">记住密码</el-checkbox>
+          <el-checkbox v-model="loginForm.rememberMe" style="margin: 0px 0px 25px 0px">{{ $t('om.login.rememberMe') }}</el-checkbox>
           <el-form-item style="width: 100%">
             <el-button :loading="loading" size="large" type="primary" style="width: 100%" @click.prevent="handleLogin">
-              <span v-if="!loading">登 录</span>
-              <span v-else>登 录 中...</span>
+              <span v-if="!loading">{{ $t('om.login.loading1') }}</span>
+              <span v-else>{{ $t('om.login.loading2') }}</span>
             </el-button>
             <div style="float: right" v-if="register">
-              <router-link class="link-type" :to="'/register'">立即注册</router-link>
+              <router-link class="link-type" :to="'/register'">{{ $t('om.login.register') }}</router-link>
             </div>
           </el-form-item>
         </el-form>
@@ -70,9 +70,9 @@ const loginForm = ref({
 });
 
 const loginRules = {
-  username: [{ required: true, trigger: 'blur', message: '请输入您的账号' }],
-  password: [{ required: true, trigger: 'blur', message: '请输入您的密码' }],
-  code: [{ required: true, trigger: 'change', message: '请输入验证码' }],
+  username: [{ required: true, trigger: 'blur', message: proxy.$t('om.login.rule1') }],
+  password: [{ required: true, trigger: 'blur', message: proxy.$t('om.login.rule2') }],
+  code: [{ required: true, trigger: 'change', message: proxy.$t('om.login.rule3') }],
 };
 
 const codeUrl = ref('');
--- src/views/register.vue
+++ src/views/register.vue
@@ -3,23 +3,31 @@
     <el-form ref="registerRef" :model="registerForm" :rules="registerRules" class="register-form">
       <h3 class="title">{{ appTitle }}</h3>
       <el-form-item prop="username">
-        <el-input v-model="registerForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
-          <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
+        <el-input v-model="registerForm.username" type="text" size="large" auto-complete="off" :placeholder="$t('om.login.username')">
+          <template #prefix>
+            <svg-icon icon-class="user" class="el-input__icon input-icon" />
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="registerForm.password" type="password" size="large" auto-complete="off" placeholder="密码" @keyup.enter="handleRegister">
-          <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
+        <el-input v-model="registerForm.password" type="password" size="large" auto-complete="off" :placeholder="$t('om.login.password1')" @keyup.enter="handleRegister">
+          <template #prefix>
+            <svg-icon icon-class="password" class="el-input__icon input-icon" />
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="confirmPassword">
-        <el-input v-model="registerForm.confirmPassword" type="password" size="large" auto-complete="off" placeholder="确认密码" @keyup.enter="handleRegister">
-          <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
+        <el-input v-model="registerForm.confirmPassword" type="password" size="large" auto-complete="off" :placeholder="$t('om.login.password2')" @keyup.enter="handleRegister">
+          <template #prefix>
+            <svg-icon icon-class="password" class="el-input__icon input-icon" />
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="code" v-if="captchaEnabled">
-        <el-input size="large" v-model="registerForm.code" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter="handleRegister">
-          <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
+        <el-input size="large" v-model="registerForm.code" auto-complete="off" :placeholder="$t('om.login.code')" style="width: 63%" @keyup.enter="handleRegister">
+          <template #prefix>
+            <svg-icon icon-class="validCode" class="el-input__icon input-icon" />
+          </template>
         </el-input>
         <div class="register-code">
           <img :src="codeUrl" @click="getCode" class="register-code-img" />
@@ -27,17 +35,17 @@
       </el-form-item>
       <el-form-item style="width: 100%">
         <el-button :loading="loading" size="large" type="primary" style="width: 100%" @click.prevent="handleRegister">
-          <span v-if="!loading">注 册</span>
-          <span v-else>注 册 中...</span>
+          <span v-if="!loading">{{ $t('om.login.loading3') }}</span>
+          <span v-else>{{ $t('om.login.loading4') }}</span>
         </el-button>
         <div style="float: right">
-          <router-link class="link-type" :to="'/login'">使用已有账户登录</router-link>
+          <router-link class="link-type" :to="'/login'">{{ $t('om.login.loading5') }}</router-link>
         </div>
       </el-form-item>
     </el-form>
     <!--  底部  -->
     <div class="el-register-footer">
-      <span>Copyright © 2018-2022 bit.vip All Rights Reserved.</span>
+      <span>Copyright © 2018-2022 {{ appTitle }} All Rights Reserved.</span>
     </div>
   </div>
 </template>
@@ -61,7 +69,7 @@ const registerForm = ref({
 
 const equalToPassword = (rule, value, callback) => {
   if (registerForm.value.password !== value) {
-    callback(new Error('两次输入的密码不一致'));
+    callback(new Error(proxy.$t('om.login.msg1')));
   } else {
     callback();
   }
@@ -69,18 +77,18 @@ const equalToPassword = (rule, value, callback) => {
 
 const registerRules = {
   username: [
-    { required: true, trigger: 'blur', message: '请输入您的账号' },
-    { min: 2, max: 20, message: '用户账号长度必须介于 2 和 20 之间', trigger: 'blur' },
+    { required: true, trigger: 'blur', message: proxy.$t('om.login.rule1') },
+    { min: 2, max: 20, message: proxy.$t('om.user.rules2'), trigger: 'blur' },
   ],
   password: [
-    { required: true, trigger: 'blur', message: '请输入您的密码' },
-    { min: 5, max: 20, message: '用户密码长度必须介于 5 和 20 之间', trigger: 'blur' },
+    { required: true, trigger: 'blur', message: proxy.$t('om.login.rule2') },
+    { min: 5, max: 20, message: proxy.$t('om.user.rules5'), trigger: 'blur' },
   ],
   confirmPassword: [
-    { required: true, trigger: 'blur', message: '请再次输入您的密码' },
+    { required: true, trigger: 'blur', message: proxy.$t('om.login.rule4') },
     { required: true, validator: equalToPassword, trigger: 'blur' },
   ],
-  code: [{ required: true, trigger: 'change', message: '请输入验证码' }],
+  code: [{ required: true, trigger: 'change', message: proxy.$t('om.login.rule3') }],
 };
 
 const codeUrl = ref('');
@@ -94,7 +102,7 @@ function handleRegister() {
       register(registerForm.value)
         .then((res) => {
           const username = registerForm.value.username;
-          ElMessageBox.alert("<font color='red'>恭喜你，您的账号 " + username + ' 注册成功！</font>', '系统提示', {
+          ElMessageBox.alert("<font color='red'>" + proxy.$t('om.login.msg2', { field: username }) + '</font>', proxy.$t('om.system_prompt'), {
             dangerouslyUseHTMLString: true,
             type: 'success',
           })
@@ -135,6 +143,7 @@ getCode();
   background-image: url('../assets/images/login-background.jpg');
   background-size: cover;
 }
+
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
@@ -146,32 +155,39 @@ getCode();
   background: #ffffff;
   width: 400px;
   padding: 25px 25px 5px 25px;
+
   .el-input {
     height: 40px;
+
     input {
       height: 40px;
     }
   }
+
   .input-icon {
     height: 39px;
     width: 14px;
     margin-left: 0px;
   }
 }
+
 .register-tip {
   font-size: 13px;
   text-align: center;
   color: #bfbfbf;
 }
+
 .register-code {
   width: 33%;
   height: 40px;
   float: right;
+
   img {
     cursor: pointer;
     vertical-align: middle;
   }
 }
+
 .el-register-footer {
   height: 40px;
   line-height: 40px;
@@ -184,6 +200,7 @@ getCode();
   font-size: 12px;
   letter-spacing: 1px;
 }
+
 .register-code-img {
   height: 40px;
   padding-left: 12px;
